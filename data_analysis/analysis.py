da71a95da51a9a3929cda8f21851a2f9ed6ebe8f
jps <j263792@dav.unicamp.br>
2023-05-17 22:51:42 -0300

    data analysis updated with modifications

--- data_analysis/analysis.py
+++ data_analysis/analysis.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ### Set your path to the folder containing the .csv files
-PATH = '/home/jps/GraphGenFrw/Simulator/logfiles/alg' # Use your path
+SIM_PATH = '/home/jps/GraphGenFrw/Simulator/'
+PATH = f'{SIM_PATH}logfiles/alg' # Use your path
 
 def latencyComparison(algorithms, users, instance):
     dataframes = []
@@ -70,11 +71,13 @@ def execTimeComparison(algorithms, users, instance):
 
 def socialWelfareComparison(algorithms, users, instance):
     dataframes = []
+    dfExact = pd.read_csv(f'{PATH}8-30users/social_welfare_8_1.csv')
     for alg in algorithms:
         df = pd.read_csv(f'{PATH}{alg[0]}-{users}users/social_welfare_{alg[0]}_{instance}.csv')
         df['algorithm'] = alg[1]
         df['time-step'] -= 1
         df['time-step'] /= 60
+        df['social welfare'] /= dfExact['social welfare']
         dataframes.append(df)
 
     # Merge dataframes using a common key, such as a timestamp
@@ -93,7 +96,7 @@ def socialWelfareComparison(algorithms, users, instance):
     # Customize the plot
     ax.set_xticks(merged_df['time-step'].unique())
     ax.set_xlabel('Optimization call Δt (every 1 minute in simulation time)')
-    ax.set_ylabel('Social Welfare ($)')
+    ax.set_ylabel('Ratio (social welfare achieved/optimal social welfare)')
     ax.set_title('Social Welfare Comparison')
     ax.legend()
     plt.savefig('social_welfare_comparison.png')
@@ -104,28 +107,33 @@ def cloudletsUsageComparison(algorithms, users, instance):
         df = pd.read_csv(f'{PATH}{alg[0]}-{users}users/cloudlets_usage_{alg[0]}_{instance}.csv')
         df['time-step'] -= 1
         df['time-step'] /= 60
-        df['algorithm'] = alg[1]
-        buildGraphForAlg(df, alg[1])
+        df['algorithm'] = alg
+        buildGraphForAlg(df, alg)
 
 def buildGraphForAlg(df, alg):
-    # buildGraphForRes(df, alg, 'used cpu', 'CPU Usage Comparison')
-    # buildGraphForRes(df, alg, 'used storage', 'Storage Usage Comparison')
-    buildGraphForRes(df, alg, 'used ram', 'RAM Usage Comparison')
+    buildGraphForRes(df, alg, 'cpu', 'CPU Usage Comparison')
+    buildGraphForRes(df, alg, 'storage', 'Storage Usage Comparison')
+    buildGraphForRes(df, alg, 'ram', 'RAM Usage Comparison')
 
 def buildGraphForRes(df, alg, res, title):
     # Create a figure and two axes objects
     fig, ax = plt.subplots(figsize=(20, 10))
 
     # Plotting the line graph with error bars
-    ax.errorbar(df['time-step'], df['used ram avg'], yerr=df['used ram std'], fmt='-o', label='Average of used ram')
+    ax.errorbar(df['time-step'], df[f'used {res} avg'], yerr=df[f'used {res} std'], fmt='-o', label=f'Average of used {res}')
 
     # Adding vertical bars representing 'unused cpu'
-    ax.bar(df['time-step'], df['unused ram'], alpha=0.5, label='Unused ram')
+    ax.bar(df['time-step'], df[f'unused {res}'], alpha=0.5, label=f'Unused {res}')
 
     # Adding labels and title to the graph
     ax.set_xlabel('Optimization call Δt (every 1 minute in simulation time)')
-    ax.set_ylabel('RAM (MB)')
-    ax.set_title('RAM usage for Greedy-Quadtree algorithm')
+    
+    if res == 'ram' or res == 'storage':
+        ax.set_ylabel(f'{res.upper()} (MB)')
+    else:
+        ax.set_ylabel('CPU (Processing units)')
+    
+    ax.set_title(f'title - {alg[1]}')
     ax.legend()
 
     # Make some labels.
@@ -138,8 +146,8 @@ def buildGraphForRes(df, alg, res, title):
             rect.get_x() + rect.get_width() / 2, height + 5, label, ha="center", va="bottom"
         )
 
-    # Display the graph
-    plt.show()
+    plt.savefig(f'{SIM_PATH}/data_analysis/res_graphs/{res}_{alg[0]}_comparison.png')
+    #plt.show()
 
 
 
@@ -182,11 +190,13 @@ def buildGraphForRes(df, alg, res, title):
 
 def pricesComparison(algorithms, users, instance):
     dataframes = []
+    dfExact = pd.read_csv(f'{PATH}8-30users/social_welfare_8_1.csv')
     for alg in algorithms:
         df = pd.read_csv(f'{PATH}{alg[0]}-{users}users/prices_{alg[0]}_{instance}.csv')
         df['algorithm'] = alg[1]
         df['time-step'] -= 1
         df['time-step'] /= 60
+        df['prices'] /= dfExact['social welfare']
         dataframes.append(df)
 
     # Merge dataframes using a common key, such as a timestamp
@@ -205,8 +215,8 @@ def pricesComparison(algorithms, users, instance):
     # Customize the plot
     ax.set_xticks(merged_df['time-step'].unique())
     ax.set_xlabel('Optimization call Δt (every 1 minute in simulation time)')
-    ax.set_ylabel('Price ($)')
-    ax.set_title('Price Comparison')
+    ax.set_ylabel('Ratio (profit achived/optimal social welfare)')
+    ax.set_title('Profit Comparison')
     ax.legend()
     plt.savefig('prices_comparison.png')
     # plt.show()
@@ -217,9 +227,9 @@ algorithms_noQT = [(1, 'Greedy'), (3, 'Cross Edge'), (4, '2-phases')]
 users = 100
 instance = 1
 
-cloudletsUsageComparison([(0, 'Greedy with Quadtree')], 30, instance)
-# latencyComparison(algorithms, users, instance)
-# execTimeComparison(algorithms, users, instance)
-# execTimeComparison(algorithms_QT, users, instance)
-# socialWelfareComparison(algorithms, users, instance)
-# pricesComparison(algorithms, users, instance)
+cloudletsUsageComparison(algorithms, users, instance)
+latencyComparison(algorithms, users, instance)
+execTimeComparison(algorithms_noQT, users, instance)
+execTimeComparison(algorithms_QT, users, instance)
+socialWelfareComparison(algorithms, users, instance)
+pricesComparison(algorithms, users, instance)
