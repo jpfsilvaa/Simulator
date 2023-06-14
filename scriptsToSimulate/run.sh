3015948207d20167c4465ef5d55bac5493486eac
jps <j263792@dav.unicamp.br>
2023-06-14 17:34:02 -0300

    updating the bash file to run the simulations with the new instance

--- scriptsToSimulate/run.sh
+++ scriptsToSimulate/run.sh
@@ -1,4 +1,4 @@
-nbUsers=(15)
+nbUsers=(25)
 
 for i in {0..5}
 do
@@ -7,7 +7,7 @@ do
         ${algIdx} \
         ${nbUsers} \
         11 \
-        /home/jps/GraphGenFrw/Simulator/GraphGen/input_files/systemInput/newInst.json \
+        /home/jps/GraphGenFrw/Simulator/GraphGen/input_files/systemInput/overflow_test25_2.json \
         /home/jps/GraphGenFrw/Simulator/GraphGen/BusMovementModel/raw_data/map_20171024.xml
     echo 'Done for '${nbUsers[0]}' users and algorithm '${algIdx}
 done
